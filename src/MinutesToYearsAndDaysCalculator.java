c099f4eabddf84fd97e64f02bb16ddeb5517ede6
XamSP <max0720x@gmail.com>
2018-12-21 12:56:12 -0500

    wok

---
+++ src/MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,17 @@
+public class MinutesToYearsAndDaysCalculator {
+
+    public static void printYearsAndDays(long minutes) {
+
+        if (minutes < 0) { System.out.println("Invalid Value");}
+
+        System.out.println()
+    }
+
+
+
+    public static void main(String[] args) {
+
+        printYearsAndDays(60);
+
+    }
+}
